53c1e338bf4b5ce2625e510fe332b95d9709a618
Andrey Esaulov <esaulov.andrey@gmail.com>
2021-08-22 20:37:06 +0200

    Updates

---
+++ precache-manifest.63aea480fb8263a3d58ea0be151f1957.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af9d585fd8c4a863a8a6",
+    "url": "/monsters-corp/static/css/main.bb3c570b.chunk.css"
+  },
+  {
+    "revision": "af9d585fd8c4a863a8a6",
+    "url": "/monsters-corp/static/js/main.3e8e6037.chunk.js"
+  },
+  {
+    "revision": "2dcce9453e8c0ed5e624",
+    "url": "/monsters-corp/static/js/runtime~main.1e666358.js"
+  },
+  {
+    "revision": "a7e5225d1f25e1be2156",
+    "url": "/monsters-corp/static/js/2.9580fe7e.chunk.js"
+  },
+  {
+    "revision": "f4445840aed9e4d44538a9bb4685fbca",
+    "url": "/monsters-corp/index.html"
+  }
+];
